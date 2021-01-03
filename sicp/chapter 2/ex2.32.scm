8e9fd474d843ca124c361be8c261e4208b8b5546
Murod Khaydarov <murod.haydarov@gmail.com>
2021-01-04 00:55:46 +0300

    update ex2.32

--- sicp/chapter 2/ex2.32.scm
+++ sicp/chapter 2/ex2.32.scm
@@ -13,3 +13,11 @@
 							(if (null? r)
 								(list (car s))
 								(append (list (car s)) r))) rest)))))
+
+;Another solution
+(define (subsets s)
+	(if (null? s)
+		(list nil)
+		(let ((rest (subsets (cdr s))))
+			(append rest (map (lambda (r)
+								(cons (car s) r)) rest)))))
