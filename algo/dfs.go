a722580ebb89c6652f6e623d089b879a981bdeaf
Murod Khaydarov <murod.haydarov@gmail.com>
2021-03-13 01:19:33 +0300

    update dfs template

--- algo/dfs.go
+++ algo/dfs.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 // TC: O(V + E), where V is the number of vertices and E is the number of edges in the graph.
-func DFS1(graph [][]int, x, y int) bool {
+func DFS(graph [][]int, x, y int) bool {
 	if x == y {
 		return true
 	}
 
 	for _, path := range graph {
 		if path[0] == x {
-			if DFS1(graph, path[1], y) == true {
+			if DFS(graph, path[1], y) == true {
 				return true
 			}
 		}
@@ -21,7 +21,7 @@ func DFS1(graph [][]int, x, y int) bool {
 
 // TC: O(V + E), where V is the number of vertices and E is the number of edges in the graph.
 // SC: O(V)
-func DFS(graph [][]int, x, y int) bool {
+func DFS1(graph [][]int, x, y int) bool {
 	var visited [100][100]int
 
 	var dfsUtil func(i, j int) bool
@@ -44,6 +44,68 @@ func DFS(graph [][]int, x, y int) bool {
 	return dfsUtil(x, y)
 }
 
+// Implementation with Stack
+// -----------
+//
+
+type Stack struct {
+	data []int
+}
+
+func (s *Stack) Push(x int) {
+	s.data = append(s.data, x)
+}
+
+func (s *Stack) Top() int {
+	if len(s.data) == 0 {
+		return -1
+	}
+
+	l := len(s.data)
+
+	return s.data[l - 1]
+}
+
+func (s *Stack) Pop() bool {
+	if len(s.data) == 0 {
+		return false
+	}
+
+	l := len(s.data)
+
+	s.data = s.data[:l-1]
+	return true
+}
+
+func (s *Stack) Empty() bool {
+	return len(s.data) == 0
+}
+
+func DFS3(graph [][]int, x, y int) bool {
+	s := Stack{}
+	s.Push(x)
+
+	var visited [10]int
+
+	for !s.Empty() {
+		v := s.Top()
+		s.Pop()
+
+		if v == y {
+			return true
+		}
+
+		visited[v] = 1
+		for _, path := range graph {
+			if path[0] == v && visited[path[1]] == 0 {
+				s.Push(path[1])
+			}
+		}
+	}
+
+	return false
+}
+
 func main() {
 	graph := [][]int{
 		{1, 2},
@@ -56,6 +118,6 @@ func main() {
 		{6, 7},
 	}
 
-	r := DFS(graph, 4, 5)
+	r := DFS3(graph, 5, 5)
 	fmt.Println(r)
 }
