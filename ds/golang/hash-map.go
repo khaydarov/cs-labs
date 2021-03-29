609e2f01849af1541905edec14a90a5b074bc64f
Murod Khaydarov <murod.haydarov@gmail.com>
2021-03-30 00:22:37 +0300

    hashtable design implementation

---
+++ ds/golang/hash-map.go
@@ -0,0 +1,57 @@
+package main
+
+type Value struct {
+	Key, Value int
+}
+
+// HashMap structure
+type HashMap struct {
+	size int
+	buckets [][]*Value
+}
+
+// Add appends the element to the bucket if it is not exist
+func (hm *HashMap) Put(key, value int) {
+	bucket := hm.getBucket(key)
+
+	updated := false
+	for _, v := range hm.buckets[bucket] {
+		if v.Key == key {
+			v.Value = value
+			updated = true
+			break
+		}
+	}
+
+	if !updated {
+		hm.buckets[bucket] = append(hm.buckets[bucket], &Value{key, value})
+	}
+}
+
+// Get returns value if key exists
+func (hm *HashMap) Get(key int) int {
+	bucket := hm.getBucket(key)
+	for _, v := range hm.buckets[bucket] {
+		if v.Key == key {
+			return v.Value
+		}
+	}
+
+	return -1
+}
+
+// Remove removes from bucket the key
+func (hm *HashMap) Remove(key int) {
+	bucket := hm.getBucket(key)
+	for _, v := range hm.buckets[bucket] {
+		if v.Key == key {
+			v.Value = -1
+			break
+		}
+	}
+}
+
+// getBucket calculates hash of the key and returns index of bucket
+func (hm *HashMap) getBucket(key int) int {
+	return key % hm.size
+}
---
+++ ds/golang/hash-set.go
@@ -0,0 +1,45 @@
+package main
+
+// HashSet structure
+type HashSet struct {
+	size int
+	buckets [][]int
+}
+
+// Add appends the element to the bucket if it is not exist
+func (hs *HashSet) Add(key int) {
+	if hs.Contains(key) {
+		return
+	}
+
+	bucket := hs.getBucket(key)
+	hs.buckets[bucket] = append(hs.buckets[bucket], key)
+}
+
+// Contains returns true if key exists
+func (hs *HashSet) Contains(key int) bool {
+	bucket := hs.getBucket(key)
+	for _, v := range hs.buckets[bucket] {
+		if v == key {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Remove removes from bucket the key
+func (hs *HashSet) Remove(key int) {
+	bucket := hs.getBucket(key)
+	for i, v := range hs.buckets[bucket] {
+		if v == key {
+			hs.buckets[bucket][i] = -1
+			break
+		}
+	}
+}
+
+// getBucket calculates hash of the key and returns index of bucket
+func (hs *HashSet) getBucket(key int) int {
+	return key % hs.size
+}
