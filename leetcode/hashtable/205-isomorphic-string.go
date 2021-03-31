5b55c3f44edbf95a8b888e79407d14ae8b448f6e
Murod Khaydarov <murod.haydarov@gmail.com>
2021-04-01 00:38:21 +0300

    lc daily

---
+++ leetcode/hashtable/205-isomorphic-string.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func isIsomorphic(s string, t string) bool {
+	transformationsST := make(map[byte]byte)
+	transformationsTS := make(map[byte]byte)
+
+	for i := 0; i < len(s); i++ {
+		_, sExists := transformationsST[s[i]]
+		_, tExists := transformationsTS[t[i]]
+
+		if (!sExists && tExists && s[i] != transformationsTS[t[i]]) || (sExists && transformationsST[s[i]] != t[i]) {
+			return false
+		}
+
+		transformationsST[s[i]] = t[i]
+		transformationsTS[t[i]] = s[i]
+	}
+
+	result := ""
+	for i := 0; i < len(s); i++ {
+		result += string(transformationsST[s[i]])
+	}
+
+	if result != t {
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(isIsomorphic("badc", "baba"))
+}
