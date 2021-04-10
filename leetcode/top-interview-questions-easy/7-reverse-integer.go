facd074d62c9bd1cf83857866345f4810ac152fb
Murod Khaydarov <murod.haydarov@gmail.com>
2021-04-10 20:25:35 +0300

    lc daily

---
+++ leetcode/top-interview-questions-easy/7-reverse-integer.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func reverse(x int) int {
+	min := -2147483648
+	max := 2147483647
+
+	sum := 0
+	for x != 0 {
+		k := x % 10
+		x /= 10
+
+		if sum > max / 10 || (sum == max / 10 && k > 7)  {
+			return 0
+		}
+
+		if sum < min / 10 || (sum == min / 10 && k < -8) {
+			return 0
+		}
+
+		sum = sum * 10 + k
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println(reverse(-74))
+}
