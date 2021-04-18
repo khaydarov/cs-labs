209a786dd3eb69479f695541100aaa61b65e99ed
Murod Khaydarov <murod.haydarov@gmail.com>
2021-04-18 23:49:46 +0300

    math & other from interview easy questions

---
+++ leetcode/top-interview-questions-easy/326-power-of-three.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Naive
+// TC: O(Log3 N)
+// SC: O(1)
+func isPowerOfThree1(n int) bool {
+	if n <= 0 {
+		return false
+	}
+
+	k := 1
+	x := 0
+	for k < n {
+		k *= 3
+		x++
+	}
+
+	return k == n
+}
+
+// Math: Max integer value is 2^31 - 1. So the max value that is power of three is 3^19: 3^log3 2^31-1 ~ 3^19,...
+// TC: O(1)
+// SC: O(1)
+func isPowerOfThree(n int) bool {
+	return n > 0 && 1162261467 % n == 0
+}
+
+func main() {
+	n := 20
+	fmt.Println(isPowerOfThree(n))
+}
